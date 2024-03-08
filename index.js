be2d30821cf21f08dfe78e2721eda0a4fc40bd28
Smark16 <sengendomark16@gmail.com>
2024-03-08 16:43:58 +0300

    phone_Gadgets

---
+++ index.js
@@ -0,0 +1,8 @@
+let form = document.querySelector('form')
+let textarea = document.querySelector('textarea').value
+let name = document.querySelector('#name').value
+let email = document.querySelector('#email')
+
+form.addEventListener('submit', (e)=>{
+e.preventDefault()
+})
